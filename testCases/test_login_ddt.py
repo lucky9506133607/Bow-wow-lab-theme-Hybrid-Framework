c28ba6c880bade466d16e434f997cd8f8bd27541
lucky9506133607 <lucky9506133607@gmail.com>
2023-06-03 22:02:12 +0530

    Change on Db_data

--- testCases/test_login_ddt.py
+++ testCases/test_login_ddt.py
@@ -1,11 +1,13 @@
 from builtins import input
+
+from mysqlx import DbDoc
 from selenium.webdriver.common.by import By
 
 import pytest
 from pageObjects.LoginPage import LoginPage
 from utilities.readProperties import ReadConfig
 from utilities.customLogger import LogGen
-from utilities import XLUtils
+from utilities import Db_data
 import time
 
 
@@ -15,7 +17,6 @@ class Test_002_DDT_Login():
     logger = LogGen.loggen()  # Logger
 
     def test_login_ddt(self, setup):
-        self.logger.info("******* Starting Test_002_DDT_Login Test **********")
         self.logger.info("******* Starting Login DDT Test **********")
         self.driver = setup
         self.driver.get(self.baseURL)
@@ -25,69 +26,12 @@ class Test_002_DDT_Login():
             self.driver.get('https://bow-wow-lab.myshopify.com/account/login')
         self.driver.maximize_window()
         self.lp = LoginPage(self.driver)
-        self.logger.info("Closed driver")
-        time.sleep(5)
 
-        # self.customers_detail = XLUtils.readData('bowwowlabdb', 'SELECT * FROM customers WHERE email = %s', params)
+
+
+        #get data from Db_data
         query = "SELECT * FROM customers WHERE email = 'ls2170184@gmail.com'"
-        self.customers_detail = XLUtils.readData('bowwowlabdb', query)
+        self.customers_detail = Db_data.readData('bowwowlabdb', query)
         self.logger.info(self.customers_detail)
-        """
-        self.lp.setUserName(self.inputusername)
-        self.lp.setPassword(self.inputpassword)
-        self.lp.clickLogin()
 
-        if len(self.customers_detail) < 0:
-            self.logger.info("******* Login failed **********")
-            self.logger.info("******* Customer already exists **********")
 
-        time.sleep(5)
-        self.driver.quit()
-        """
-
-        """self.rows = XLUtils.getRowCount(self.path, 'Sheet1')
-        print('Number of rows...',self.rows)
-        lst_status=[]
-
-        for r in range(2,self.rows+1):
-            self.user=XLUtils.readData(self.path,'Sheet1',r,1)
-            self.password = XLUtils.readData(self.path, 'Sheet1', r, 2)
-            self.exp = XLUtils.readData(self.path, 'Sheet1', r, 3)
-
-            self.lp.setUserName(self.user)
-            self.lp.setPassword(self.password)
-            self.lp.clickLogin()
-            time.sleep(5)
-
-            act_title=self.driver.title
-            exp_title="Dashboard / nopCommerce administration"
-
-            if act_title==exp_title:
-                if self.exp=='Pass':
-                    self.logger.info("**** passed ****")
-                    self.lp.clickLogout();
-                    lst_status.append("Pass")
-                elif self.exp=='Fail':
-                    self.logger.info("**** failed ****")
-                    self.lp.clickLogout();
-                    lst_status.append("Fail")
-
-            elif act_title!=exp_title:
-                if self.exp == 'Pass':
-                    self.logger.info("**** failed ****")
-                    lst_status.append("Fail")
-                elif self.exp == 'Fail':
-                    self.logger.info("**** passed ****")
-                    lst_status.append("Pass")
-            print(lst_status)
-        if "Fail" not in lst_status:
-            self.logger.info("******* DDT Login test passed **********")
-            self.driver.close()
-            assert True
-        else:
-            self.logger.error("******* DDT Login test failed **********")
-            self.driver.close()
-            assert False
-
-        self.logger.info("******* End of Login DDT Test **********")
-        self.logger.info("**************** Completed  TC_LoginDDT_002 ************* ");"""
